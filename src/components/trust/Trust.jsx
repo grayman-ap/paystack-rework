1b2cb5403fcafd859efdfa21aef3f1e7f99d4d4f
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2023-01-06 19:38:58 +0100

    trust is key

--- src/components/trust/Trust.jsx
+++ src/components/trust/Trust.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import trustData from "../../fixtures/trust.json";
 export default function Trust() {
   return (
+    <>
     <div className="trust">
     <TrustFrame className="trust__frame">
       <TrustPane className="trust__pane">
@@ -17,7 +18,7 @@ export default function Trust() {
           </TrustCaption>
         </TrustHeader>
         <TrustCard className="trust__card">
-          {trustData.map((item) => (
+          {trustData.item.map((item) => (
            
                 <TrustItem className="card_item">
               <ImageContainer className="img__cont">
@@ -49,6 +50,34 @@ export default function Trust() {
       </TrustPane>
     </TrustFrame>
     </div>
+
+
+    <div className="trust">
+    <TrustFrame className="growth__frame">
+      <TrustPane className="trust__pane flex">
+      <TrustContent className="trust__content">
+        <TrustHeader className="trust__header">
+          <TrustTitle className="trust__title">
+            Powering growth for amazing businesses
+          </TrustTitle>
+          <TrustCaption className="trust__caption">
+          Paystack is a growth engine for a new generation of innovative, forward-looking organizations operating in Africa.
+          </TrustCaption>
+        </TrustHeader>
+        <div className="contain">
+          {trustData.item2.map((item) => (
+           <div className="display-grid">
+           
+                <img className="growth__image" src={item.image} />
+            
+           </div>
+          ))}
+       </div>
+        </TrustContent>
+      </TrustPane>
+    </TrustFrame>
+    </div>
+    </>
   );
 }
 const TrustFrame = styled.div``;
